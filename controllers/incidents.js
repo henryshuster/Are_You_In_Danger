c908adfc2212357f1788a94ca3625fdf2ae43536
TylerMas19 <tyler.masuyama19@trinityschoolnyc.org>
2019-05-10 14:42:06 -0400

    ye

--- controllers/incidents.js
+++ controllers/incidents.js
@@ -3,21 +3,25 @@ var app = express.Router();
 
 var global_incident_type;
 
-app.get('/incident/:id/shooting/', function(request,response){
-  console.log('GET Request- Shooting Incident');
+app.post('/incident/:id/shooting', function(request,response){
+  console.log('POST Request- Shooting Incident');
   var user={
     "email": request.params.id,
+    "longtitude": request.body.longitude,
+    "latitude": request.body.latitude
   }
-  global_incident_type=0;
+  global_incident_type=1;
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
   response.render('incident', {incident_type: 0, save_feedback: 0, user});
 });
 
-app.get('/incident/:id/arrest', function(request,response){
-  console.log('GET Request- Arrest Incident');
+app.post('/incident/:id/arrest', function(request,response){
+  console.log('POST Request- Arrest Incident');
   var user={
     "email": request.params.id,
+    "longtitude": request.body.longitude,
+    "latitude": request.body.latitude
   }
   global_incident_type=1;
   response.status(200);
